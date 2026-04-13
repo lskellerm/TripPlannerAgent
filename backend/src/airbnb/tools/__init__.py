0d44868aa5d1f069c5ccc59680208cbbc6abd286
Luis Kellermann <lekellerm95@gmail.com>
2026-04-13 12:22:00 -0600

    feat(tools): add explore_listings and filter_search_results to module exports

--- backend/src/airbnb/tools/__init__.py
+++ backend/src/airbnb/tools/__init__.py
@@ -4,8 +4,10 @@ from src.airbnb.tools.analysis import (
 	calculate_cost_breakdown,
 	calculate_trip_totals,
 	filter_listings,
+	filter_search_results,
 	rank_by_category,
 )
+from src.airbnb.tools.exploration import explore_listings
 from src.airbnb.tools.parsers import (
 	parse_booking_price,
 	parse_listing_details,
@@ -19,7 +21,9 @@ __all__: list[str] = [
 	"build_search_url",
 	"calculate_cost_breakdown",
 	"calculate_trip_totals",
+	"explore_listings",
 	"filter_listings",
+	"filter_search_results",
 	"parse_booking_price",
 	"parse_listing_details",
 	"parse_listing_page",
